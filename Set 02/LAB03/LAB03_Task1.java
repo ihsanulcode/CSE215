b2f18aa84ae538e8f6ece78dfe379a841d8c4bc7
Ihsanul Haque Asif <66785935+ihsanulcode@users.noreply.github.com>
2021-03-06 22:25:14 +0600

    Update LAB03_Task1.java

--- Set 02/LAB03/LAB03_Task1.java
+++ Set 02/LAB03/LAB03_Task1.java
@@ -7,16 +7,14 @@ public class LAB03_Task1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
-		
-		System.out.print("Enter n: ");
-		int n = input.nextInt();
-		input.close();
-		
-		//generate random number Range: 5-20 then print 
-		for(int i=0;i<=n;i++) {
-			int Rnum = (int)(Math.random()*(20-5+1)+5);
-			System.out.print(Rnum+" ");
+
+		//generate random number Range : 5-20
+		int n = (int) (Math.random() * (20 - 5 + 1) + 5);
+
+		// print 0-n
+		for (int i = 0; i <= n; i++) {
+
+			System.out.println(i + " ");
 		}
 	}
 
