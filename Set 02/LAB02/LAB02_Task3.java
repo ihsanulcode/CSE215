7e45c31ece14d035143e79e7745aeadcfe8fb57e
Ihsanul Haque Asif <66785935+ihsanulcode@users.noreply.github.com>
2021-03-08 01:09:04 +0600

    Solve

---
+++ Set 02/LAB02/LAB02_Task3.java
@@ -0,0 +1,14 @@
+//Write a program that prints the multiplication table from 1 to 5
+public class LAB02_Task3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=10;j++) {
+				System.out.println(""+i+" X "+j+" = "+(i*j));
+			}System.out.println("-------------");
+		}
+	}
+
+}
